96f23b1ba8ad7362da74f37ae5a299de1e9a0bf1
edicomun <elia.dicomun@gmail.com>
2015-05-07 23:07:35 +0200

    list sudokus
    sudokus get listed in level room.

---
+++ www/js/level_room.js
@@ -0,0 +1,38 @@
+function getSearchParameters() {
+              var prmstr = window.location.search.substr(1);
+              return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+        }
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+
+function getDifficulty() {
+    var difficulty = params['difficulty'];
+    return difficulty;
+}
+
+function displaySudokus() {
+    var difficulty = getDifficulty();
+    var sudokus = JSON.parse(window.localStorage.getItem('sudokus')).sudokus;
+
+    for (var i = 0; i < sudokus.length; i++) {
+        if (sudokus[i].difficulty == difficulty) {
+            document.write("<fieldset>");
+            document.write("<p>Id: <a href='sudoku_level.html?id="+sudokus[i].id+"'>"+sudokus[i].id+"</a></p>");
+            document.write("<p>Zeit: "+sudokus[i].time+"</p>");
+            document.write("<p>Fehler: "+sudokus[i].amountMistakes+"</p>");
+            document.write("<p>Status: "+sudokus[i].state+"</p>");
+            document.write("</fieldset>");
+        };
+    };
+}
+
+var params = getSearchParameters();
